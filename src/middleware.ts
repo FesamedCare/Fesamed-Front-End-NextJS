59cfdebdee5f82ae4532461c7fe090efadfcebb8
David <garxia0710@gmail.com>
2024-11-25 12:41:31 -0500

    feat(avoid-routes): login/register routes excluded to authenticated users

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,24 @@ import { isUserAuthenticated } from "@/session";
 import { NextRequest, NextResponse } from "next/server";
 
 const protectedRoutes = ["/dashboard"];
+
 export default function middleware(req: NextRequest) {
-  if (
-    !isUserAuthenticated &&
-    protectedRoutes.includes(req?.nextUrl?.pathname)
-  ) {
-    const absoluteUrl = new URL("/login", req.nextUrl.origin);
-    return NextResponse.redirect(absoluteUrl.toString());
+  const { pathname } = req.nextUrl;
+
+  // Si el usuario no está autenticado e intenta acceder a una ruta protegida
+  if (!isUserAuthenticated && protectedRoutes.includes(pathname)) {
+    const loginUrl = new URL("/login", req.nextUrl.origin);
+    return NextResponse.redirect(loginUrl.toString());
   }
+
+const avoidRoutes = ["/login", "/register"];
+
+  // Si el usuario está autenticado e intenta acceder a la página de inicio de sesión
+  if (isUserAuthenticated && avoidRoutes.includes(pathname)) {
+    const dashboardUrl = new URL("/dashboard", req.nextUrl.origin);
+    return NextResponse.redirect(dashboardUrl.toString());
+  }
+
+  // Continuar con la solicitud normalmente
   return NextResponse.next();
 }
