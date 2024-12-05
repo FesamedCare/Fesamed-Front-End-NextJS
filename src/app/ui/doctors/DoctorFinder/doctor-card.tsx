03242dbf2acc390c8e5e864dd5552f30c4e4f073
David <garxia0710@gmail.com>
2024-12-04 19:53:52 -0500

    feat:Search doctors page done 🔍👩🏻‍⚕️ (just client side, awaiting for doctors data base endpoints)

---
+++ src/app/ui/doctors/DoctorFinder/doctor-card.tsx
@@ -0,0 +1,37 @@
+import { Card, CardContent } from "@/components/ui/card"
+import Image from "next/image"
+
+interface DoctorCardProps {
+  name: string
+  specialty: string
+  clinic: string
+  imageUrl: string
+}
+
+export function DoctorCard({ name, specialty, clinic, imageUrl }: DoctorCardProps) {
+  return (
+    <Card className="w-full">
+      <CardContent className="p-4">
+        <div className="flex flex-col items-center text-center">
+          <div className="relative w-32 h-32 mb-4">
+            <Image
+                src={imageUrl}
+                alt={name}
+                className="rounded-lg object-cover"
+                height={400}
+                width={400}
+                priority
+              />
+          </div>
+          <div className="space-y-2">
+            <p className="text-blue-600 font-medium">{clinic}</p>
+            <h3 className="font-semibold text-lg">{name}</h3>
+            <p className="text-gray-600">{specialty}</p>
+            <p className="text-gray-400">Reseñas</p>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,113 +1,109 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    extend: {
-      colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-        transitionProperty: {
-          'object-fit': 'object-fit'
-        },
-        transitionTimingFunction: {
-          'ease-in-out': 'ease-in-out'
-        },
-        transitionDuration: {
-          '500': '500ms'
-        },
-        height: {
-          '120': '30rem', 
-        }
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
-    },
-    boxShadow:{
-      navbar: '0px 10px 8px 0px rgba(3,3,4,0.03), 0 1px 2px -1px rgba(3,3,4,0.03)',
-      "2xl":'0 0px 40px 0px rgba(0, 0, 0, 0.1)',
-    },
-    screens: {
-      sm: "640px",
-      // => @media (min-width: 640px) { ... }
-
-      md: "768px",
-      // => @media (min-width: 768px) { ... }
-
-      lg: "1090px",
-      // => @media (min-width: 1024px) { ... }
-
-      xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
-
-      "2xl": "1536px",
-      // => @media (min-width: 1536px) { ... }
-
-      "3xl": "1720px",
-      // => @media (min-width: 1536px) { ... }
-
-      "4xl": "1856px",
-      // => @media (min-width: 1536px) { ... }
+  	container: {
+  		center: 'true',
+  		padding: '2rem',
+  		screens: {
+  			'2xl': '1400px'
+  		}
+  	},
+  	extend: {
+  		colors: {
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			transitionProperty: {
+  				'object-fit': 'object-fit'
+  			},
+  			transitionTimingFunction: {
+  				'ease-in-out': 'ease-in-out'
+  			},
+  			transitionDuration: {
+  				'500': '500ms'
+  			},
+  			height: {
+  				'120': '30rem'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		}
+  	},
+  	boxShadow: {
+  		navbar: '0px 10px 8px 0px rgba(3,3,4,0.03), 0 1px 2px -1px rgba(3,3,4,0.03)',
+  		'2xl': '0 0px 40px 0px rgba(0, 0, 0, 0.1)'
+  	},
+  	screens: {
+  		sm: '640px',
+  		md: '768px',
+  		lg: '1090px',
+  		xl: '1280px',
+  		'2xl': '1536px',
+  		'3xl': '1720px',
+  		'4xl': '1856px'
+  	}
   },
-  },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
