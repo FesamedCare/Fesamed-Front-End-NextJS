dafd71a014c99411db97cc7e1b76dc8b7e987871
David <garxia0710@gmail.com>
2024-12-25 22:26:35 -0500

    working on 3d model position, breadcrumb addded to profile

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import ClientDashboard from "../ui/dashboard/client-dashboard";
+import Footer from "../ui/navigation/footer";
 
 export default function Page() {
     return (
         <div>
             <ClientDashboard />
+            <Footer/>
         </div>
     );
 }
